2dbd8a155610d32f660358dcccc2020668f6d7ab
Tickthehero1992 <43855735+Tickthehero1992@users.noreply.github.com>
2020-07-22 12:43:16 +0300

    Initial commit

---
+++ BackUpper/Form1.Designer.cs
@@ -0,0 +1,169 @@
+﻿namespace BackUpper
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.выбратьПапкуToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.инфоToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.выбратьПапкуToolStripMenuItem,
+            this.инфоToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // выбратьПапкуToolStripMenuItem
+            // 
+            this.выбратьПапкуToolStripMenuItem.Name = "выбратьПапкуToolStripMenuItem";
+            this.выбратьПапкуToolStripMenuItem.Size = new System.Drawing.Size(101, 20);
+            this.выбратьПапкуToolStripMenuItem.Text = "Выбрать папку";
+            this.выбратьПапкуToolStripMenuItem.Click += new System.EventHandler(this.выбратьПапкуToolStripMenuItem_Click);
+            // 
+            // инфоToolStripMenuItem
+            // 
+            this.инфоToolStripMenuItem.Name = "инфоToolStripMenuItem";
+            this.инфоToolStripMenuItem.Size = new System.Drawing.Size(51, 20);
+            this.инфоToolStripMenuItem.Text = "Инфо";
+            this.инфоToolStripMenuItem.Click += new System.EventHandler(this.инфоToolStripMenuItem_Click);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(50, 354);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(164, 57);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Start";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 39);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(403, 20);
+            this.textBox1.TabIndex = 2;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.button2.Location = new System.Drawing.Point(438, 39);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "ввести";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(688, 41);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 4;
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.Location = new System.Drawing.Point(598, 354);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(148, 57);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "Stop";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(542, 42);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(140, 16);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Период сохранения:";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.menuStrip1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "BackApper";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem выбратьПапкуToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem инфоToolStripMenuItem;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ BackUpper/Form1.cs
@@ -0,0 +1,192 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+using System.IO;
+
+namespace BackUpper
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            textBox2.KeyPress += textBox2_KeyPress;
+            textBox2.Text = TimeToApp.ToString();
+        }
+
+
+        private string folderNameIn="";
+        private string folderNameOut;
+        int TimeToApp=10;
+        private void выбратьПапкуToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+           
+            FolderBrowserDialog folderBrowserDialog1 = new FolderBrowserDialog();
+            DialogResult result = folderBrowserDialog1.ShowDialog();
+            folderBrowserDialog1.ShowNewFolderButton = false;
+            if (result == DialogResult.OK)
+            {
+                folderNameIn=folderBrowserDialog1.SelectedPath;
+                MessageBox.Show(folderNameIn,"Папка с данными сохранений");
+                textBox1.ForeColor = Color.Black;
+                textBox1.Text = folderNameIn;
+                textBox1.Enabled = false;
+                textBox1.BackColor = Color.LightGray;
+            }
+        }
+
+        private void textBox2_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            // Verify that the pressed key isn't CTRL or any non-numeric digit
+            if (!char.IsControl(e.KeyChar) && !char.IsDigit(e.KeyChar) && (e.KeyChar != '.'))
+            {
+                e.Handled = true;
+            }
+
+            // If you want, you can allow decimal (float) numbers
+            if ((e.KeyChar == '.') && ((sender as TextBox).Text.IndexOf('.') > -1))
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void инфоToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Выберите папку. В строке время, укажите вреся в минутах для Бэкапа");
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (folderNameIn=="")
+            {
+                MessageBox.Show("Выберите папку пути");
+                return;
+            }
+            TimeToApp = Int32.Parse(textBox2.Text);
+            FolderBrowserDialog folderBrowserDialog1 = new FolderBrowserDialog();
+            DialogResult result = folderBrowserDialog1.ShowDialog();
+            if (result == DialogResult.OK)
+            {
+                string[] nameDir = folderNameIn.Split('\\');
+                    
+                folderNameOut = folderBrowserDialog1.SelectedPath+"\\BackApps\\" +nameDir.Last();
+                MessageBox.Show(folderNameOut);
+                DirectoryInfo dirInfo = new DirectoryInfo(folderNameOut);
+                if (!dirInfo.Exists)
+                {
+                   Directory.CreateDirectory(folderNameOut);
+                    
+                }
+                CopyDir(folderNameIn, folderNameOut, true);
+                MessageBox.Show("Начинаю сохранение бэкаппов в вашу папку", "Startanul");
+                textBox2.Enabled = false;
+                textBox2.BackColor = Color.Gray;
+                button1.Enabled = false;
+                button1.BackColor = Color.Gray;
+                CopiingTimer();
+            }
+        }
+        private static System.Windows.Forms.Timer aTimer = new System.Windows.Forms.Timer();
+        public void CopiingTimer()
+        {
+            aTimer.Stop();
+            aTimer.Enabled = false;
+            aTimer.Interval = TimeToApp*60000;
+            aTimer.Tick += new EventHandler(Cooping);
+            aTimer.Enabled = true;
+        }
+        private void Cooping(object ob, EventArgs e)
+        {
+            CopyDir(folderNameIn,folderNameOut,true);
+
+        }
+        private void CopyDir(string pathIn, string pathOut,bool Subdir)
+        {
+            DirectoryInfo dir = new DirectoryInfo(pathIn);
+            FileInfo[] files = dir.GetFiles();
+            DirectoryInfo[] dirs = dir.GetDirectories();
+
+            if (!Directory.Exists(pathOut))
+            {
+                Directory.CreateDirectory(pathOut);
+            }
+
+            foreach (FileInfo file in files)
+            {
+                string temppath = Path.Combine(pathOut, file.Name);
+                file.CopyTo(temppath, true);
+            }
+            if (Subdir)
+            {
+                foreach (DirectoryInfo d in dirs)
+                {
+                    string temppath = Path.Combine(pathOut, d.Name);
+                    CopyDir(d.FullName, temppath, Subdir);
+                }
+            }
+        }
+
+        private void ввестиПапкуВручнуюToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            folderNameIn = textBox1.Text;
+            if (textBox1.Text.Length == 0)
+            {
+                return;
+            }
+            DirectoryInfo dir = new DirectoryInfo(folderNameIn);
+            if (dir.Exists)
+            {
+                MessageBox.Show(folderNameIn, "Отсюда берем");
+                textBox1.Enabled = false;
+                textBox1.BackColor = Color.LightGray;
+            }
+            else 
+            {
+                MessageBox.Show("Некорректный путь", "Ошибка");
+            }
+            
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            aTimer.Enabled = true;
+            textBox1.Enabled = true;
+            textBox2.Enabled = true;
+            textBox1.BackColor = Color.White ;
+            textBox2.BackColor = Color.White;
+            button1.Enabled = true;
+            button1.BackColor=System.Drawing.SystemColors.ActiveCaption;
+        }
+    }
+    
+}
